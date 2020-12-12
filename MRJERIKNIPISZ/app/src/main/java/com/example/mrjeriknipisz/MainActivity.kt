41b1e222db85855fd48da23f90d306d94eb8c3f0
SAIFUR_RAHMAN <71123454+saifurrahman5411@users.noreply.github.com>
2020-12-12 22:41:17 +0700

    tambah data baruuuuuu

--- MRJERIKNIPISZ/app/src/main/java/com/example/mrjeriknipisz/MainActivity.kt
+++ MRJERIKNIPISZ/app/src/main/java/com/example/mrjeriknipisz/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.mrjeriknipisz
 
 import android.content.Intent
+import android.graphics.Color.parseColor
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val REQUEST_CODE = 100
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         val btn_intent_with_data = findViewById<Button>(R.id. btn_intent_with_data)
         val btn_intent_with_parceable = findViewById<Button>(R.id.btn_intent_with_parceable)
         val btn_implicit_intent = findViewById<Button>(R.id. btn_implicit_intent)
-        val intent_with_result = findViewById<Button>(R.id.intent_with_result)
+        val intent_with_result = findViewById<Button>(R.id.btn_intent_with_result)
 
         btn_simple_intent.setOnClickListener{
             val simpleIntent = Intent(this@MainActivity, SimpleActivity::class.java)
@@ -31,6 +37,21 @@ class MainActivity : AppCompatActivity() {
             val phoneNumber = "092154557908"
             val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
             startActivity(intent)
+        }
+
+        btn_intent_with_result.setOnClickListener {
+            val intent = Intent(this@MainActivity, ResultActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == 200){
+           val color = data?.getStringExtra(ResultActivity.EXTRA_COLOR)
+          view_result.setBackgroundColor(parseColor(color))
         }
     }
 }
